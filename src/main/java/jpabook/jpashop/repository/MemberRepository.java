88a9d1b1e7395b3f84d9da0d28770f71efb704fb
dddochi <86943499+dddochi@users.noreply.github.com>
2024-05-07 16:34:53 +0900

    Create member repository & service

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em; //JPA 사용 위해 Entity Manager
+                                //@PersistenceContext : Spring이 자동으로 em을 뿌려준다
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();//JPQL 작성해야함 - parameter : JPQL, return type
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
