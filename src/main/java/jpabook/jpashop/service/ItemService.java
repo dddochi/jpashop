edad7f897dcda3700fe7cc5514f7ee86bae50440
dddochi <86943499+dddochi@users.noreply.github.com>
2024-05-15 21:17:44 +0900

    update item

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    //변경감지 기능 사용
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId); // findOne으로 찾아왔기 때문에 얘는 영속성 상태!
+        //값만 세팅해주면 되고, 따로 save()호출 안해도 JPA가 자동으로 db에 반영
+        //코드가 다 끝나고, @Transactional에 의해 commit이 된다! -> JPA는 flush를 날 -> 변경된 애를 다 찾아서 업데이트 db에 날림
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems(){
